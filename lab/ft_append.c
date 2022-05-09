080d4d4c53a3d0edebbabe400507f041d3a6cf76
Riyad Mney <rimney@e2r10p6.1337.ma>
2022-05-09 18:03:57 +0100

    ayoub >>>>

--- lab/ft_append.c
+++ lab/ft_append.c
@@ -3,48 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   ft_append.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
+/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:23:26 by rimney            #+#    #+#             */
-/*   Updated: 2022/05/08 19:19:02 by rimney           ###   ########.fr       */
+/*   Updated: 2022/05/09 18:03:10 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// void	ft_append(char **argv, char *str, int fd)
-// {
-//     if(fd < 0)
-//     {
-//         printf("file discreptor error\n");
-//         return ;
-//     }
-//     fd = open(W_ONLY)
-// }
-
-int	main(int argc, char **argv)
+void	ft_single_append(int argc, char **argv)
 {
 	int fd;
 	char *line;
 	if(argc == 3 && ft_strcmp(argv[1], ">>") == 0)
 	{
-		fd = open(argv[2], O_RDWR);
-		if(fd == -1)
-		{
-			fd = open(argv[2], O_CREAT, 0644);
-		}
+		// fd = open(argv[2], O_RDWR);
+		// if(fd == -1)
+			fd = open(argv[2], O_CREAT | O_RDWR, 0644);
 		while(1)
 		{
 			fd = open(argv[2], O_RDWR | O_APPEND, 0644);
 			line = readline("");
 			write(fd, line, strlen(line));
 			write(fd, "\n", 1);
+			free(line);
 		}
 	}
+
+}
+
+void	ft_advanced_append(int argc, char **argv, char **envp)
+{
+	int i;
+	int	fd;
+	char **cmd_parser;
+	i = 1;
+	
+	while(i < argc)
+	{
+		if(ft_strcmp(argv[i], ">>") == 0)
+		{
+			cmd_parser = ft_split(argv[i - 1], ' ');
+			//printf("%s\n", cmd_parser[0]);
+			fd = open(argv[i + 1], O_CREAT | O_RDWR, 0644);
+			dup2(fd, STDOUT_FILENO);
+			execve("/bin/ls", cmd_parser, envp);
+		}
+		i++;
+	}
+	 
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int fd;
+	char *line;
+	if(argc == 3 && ft_strcmp(argv[1], ">>") == 0)
+		ft_single_append(argc, argv);
 	else
 	{
-		fd = open(argv[1], O_RDWR | O_APPEND);
-		write(fd, "test\n", 5);
+		ft_advanced_append(argc, argv, envp);
 	}
+//	system("leaks a.out");
 	return (0);
 }
