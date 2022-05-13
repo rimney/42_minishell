255ab0492f9aeabadfa9d83e0e144a8d361dacba
Riyad Mney <rimney@e1r6p7.1337.ma>
2022-05-13 22:10:50 +0100

    last edit

--- lab/files/ft_pipe.c
+++ lab/files/ft_pipe.c
@@ -3,53 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_pipe.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
+/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 21:31:21 by rimney            #+#    #+#             */
-/*   Updated: 2022/05/13 19:02:18 by rimney           ###   ########.fr       */
+/*   Updated: 2022/05/13 20:43:56 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 
-void    pipe(char **argv, int index, int *fd)
+void    pipe_exec(char **argv, int index, int fd[2], char **envp)
 {
-    int pid;
-    char **cmd1_parser;
-    char **cmd2_parser;
+    // int pid;
+    // char **cmd1_parser;
+    // char **cmd2_parser;
 
-    cmd1_parser = ft_split(argv[index - 1], ' ');
-    cmd2_parser = ft_split(argv[index + 1], ' ');
-    if(pipe[fd) == -1])
-        perror("minishell");
-        pid = fork();
-    if(pid == 0)
-    {
-        dup2(fd[1], 1);
-        execve(ft_exec_command(argv[index - 1], envp, argv), cmd1_parser, envp);
-    }
-    close(fd[1]);
-    dup2(fd[0], 0);   
-    execve(ft_exec_command(argv[index + 1], envp, argv), cmd2_parser, envp);
-    close(fd[0]);
-     waitpid(pid, 0, 0);
+    // cmd1_parser = ft_split(argv[index - 1], ' ');
+    // cmd2_parser = ft_split(argv[index + 1], ' ');
+    // pid = fork();
+    // if(pid == 0)
+    // {
+    //     dup2(fd[1], 1);
+    //     execve(ft_exec_command(argv[index - 1], envp, argv), cmd1_parser, envp);
+    // }
+    // close(fd[1]);
+    // dup2(fd[0], 0);   
+    // execve(ft_exec_command(argv[index + 1], envp, argv), cmd2_parser, envp);
+    // close(fd[0]);
+    //  waitpid(pid, 0, 0);
+    printf("DD\n");
 }
 
 void    ft_pipe(int argc, char **argv, char **envp, int pipes)
 {
     int *fd[2];
     int i;
+    int j;
 
-    i = 1;
+    j = 0;
     *fd = malloc(sizeof(int *) * pipes);
+    while(j < pipes)
+        pipe(fd[j++]);
+    j  = 0;
+    i = 1;
     while(i < argc)
     {
-        if(pid == 0)
+        if(ft_strcmp(argv[i], "|") == 0)
         {
-            dup2(fd[1], 1);
-            execve(ft_exec_command(argv[index - 1], envp, argv), cmd1_parser, envp);
+            pipe_exec(argv, i, fd[j], envp);
+            j++;
         }
+        i++;
+    }
 }
 
 
@@ -69,7 +75,7 @@ void    ft_pipes(int argc, char **argv, char **envp)
     }
     if(npipes > 1)
     {
-        ft_pipes(argc, argv, npipes)
+        ft_pipe(argc, argv, envp, npipes);
     }
     printf("%d P<\n", npipes);
 }
